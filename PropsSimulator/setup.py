aa9c3d556feeb96501a2b6ed9bb0f09f362399e1
dschaub95 <schaub.darius@gmail.com>
2023-02-13 14:16:15 +0000

    revert autoformatting

--- PropsSimulator/setup.py
+++ PropsSimulator/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize(["*.pyx"]))
+setup(
+    ext_modules = cythonize(["*.pyx"])
+)
--- utils/utils_fn_others.py
+++ utils/utils_fn_others.py
@@ -6,43 +6,33 @@ import scanpy as sc
 
 import scipy
 
-
 def convert_h5ad_to_df(adata):
-    if type(adata.X) == np.ndarray:
-        X = adata.X
-    elif type(adata.X) == np.matrix:
-        X = np.array(adata.X)
-    elif (
-        type(adata.X) == scipy.sparse.csr.csr_matrix
-        or type(adata.X) == scipy.sparse.coo.coo_matrix
-    ):
-        X = np.array(adata.X.todense())
+    if type(adata.X)==np.ndarray:
+        X=adata.X
+    elif type(adata.X)==np.matrix:
+        X=np.array(adata.X)
+    elif type(adata.X)==scipy.sparse.csr.csr_matrix or type(adata.X)==scipy.sparse.coo.coo_matrix:
+        X=np.array(adata.X.todense())
     else:
-        sys.exit(
-            "adata.X is of type {} which is not implemented.".format(type(adata.X))
-        )
+        sys.exit("adata.X is of type {} which is not implemented.".format(type(adata.X)))
     genes = adata.var_names.tolist()
     df = pd.DataFrame(X, columns=genes, index=adata.obs["cell.type"].tolist())
     if "subject" in adata.obs.columns:
-        subjects = pd.DataFrame(
-            adata.obs.subject.tolist(), index=list(range(df.shape[0]))
-        )
+        subjects = pd.DataFrame(adata.obs.subject.tolist(), index=list(range(df.shape[0])))
         return df, df_subjects
     else:
         return df
 
-
 def save_to_csv(df, savepath, savename, sep, remove_duplicates=True, genes_in_row=True):
     if remove_duplicates:
-        df = df.loc[:, ~df.columns.duplicated(keep="first")]
+        df = df.loc[:,~df.columns.duplicated(keep="first")]
     celltypes = df.index.tolist()
     df.index = list(range(df.shape[0]))
     df_celltypes = pd.DataFrame(celltypes, index=df.index)
     if genes_in_row:
         df = df.T
-    df.to_csv(os.path.join(savepath, savename + "_counts.txt"), sep)
-    df_celltypes.to_csv(os.path.join(savepath, savename + "_celltypes.txt"), sep)
-
+    df.to_csv(os.path.join(savepath, savename+"_counts.txt"), sep)
+    df_celltypes.to_csv(os.path.join(savepath, savename+"_celltypes.txt"), sep)
 
 def save_sc(ref, savedir, method):
     savepath = os.path.join(savedir, method)
@@ -55,28 +45,27 @@ def save_sc(ref, savedir, method):
     adata = sc.read(ref)
     if "subject" in adata.obs.columns:
         df, df_subjects = convert_h5ad_to_df(adata)
-        df_subjects.to_csv(os.path.join(savepath, savename + "_subjects.txt"), sep=",")
+        df_subjects.to_csv(os.path.join(savepath, savename+"_subjects.txt"), sep=",")
     else:
         df = convert_h5ad_to_df(adata)
-    if method == "CS_datasets":
-        sep = "\t"
+    if method=="CS_datasets":
+        sep="\t"
     else:
-        sep = ","
-    if "method" == "CS_datasets":
+        sep=","
+    if "method"=="CS_datasets":
         savename = savename + "_cs"
     save_to_csv(df, savepath, savename, sep)
 
-
 def save_test(test_path, test_format, savedir, method, remove_duplicates=True):
-    if test_format == "txt":
+    if test_format=="txt":
         df = pd.read_table(test_path, index_col=0)
     if remove_duplicates:
         df = df.loc[~df.index.duplicated(keep="first")]
-    if method == "CS_datasets":
-        sep = "\t"
-        df.index.name = "GeneSymbol"
+    if method=="CS_datasets":
+        sep="\t"
+        df.index.name="GeneSymbol"
     else:
-        sep = ","
+        sep=","
     savepath = os.path.join(savedir, method)
     if "/" in test_path:
         if "h5ad" in test_path:
@@ -88,6 +77,6 @@ def save_test(test_path, test_format, savedir, method, remove_duplicates=True):
             savename = test_path.split(".h5ad")[0]
         else:
             savename = test_path.split(".txt")[0]
-    if "method" == "CS_datasets":
+    if "method"=="CS_datasets":
         savename = savename + "_cs"
-    df.to_csv(os.path.join(savepath, savename + "_test.txt"), sep)
+    df.to_csv(os.path.join(savepath, savename+"_test.txt"), sep)
