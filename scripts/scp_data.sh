402d7f2dc57a989ba8622bc9e7b17a7e41a5af61
dschaub95 <schaub.darius@gmail.com>
2023-03-09 16:33:41 +0000

    add script to copy data from server

---
+++ scripts/scp_data.sh
@@ -0,0 +1,5 @@
+# copy data from bonn epyc
+scp -r dschaub@bonn-epyc:/data/projects/robin/data/svg_align/validationData/kidney_slideSeq_v2 ./data/spatial
+
+# copy data from bonn-pe940
+# dschaub@bonn-epyc
