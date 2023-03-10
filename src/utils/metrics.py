ccbf3f790ebc955bfd3d979d865ec7a3c30da065
dschaub95 <schaub.darius@gmail.com>
2023-03-10 08:56:08 +0000

    restructure utils to eval results

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,11 +1,4 @@
 import numpy as np
-import torch
-
-
-def seed_everything(seed=42):
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
 
 
 def calc_mean_corr(x, y, transpose=True):
@@ -27,7 +20,7 @@ def calc_mean_corr_df(df_1, df_2, transpose=True, verbose=2, exclude_cols=None):
         for k, col in enumerate(df_1.columns):
             print(f"Correlation {col}: {corrs[k]}")
     if verbose > 0:
-            print(f"Mean Correlation: {mean_corr}")
+        print(f"Mean Correlation: {mean_corr}")
     return mean_corr, corrs
 
 
@@ -37,22 +30,53 @@ def get_corr(df1, df2, col):
     return r
 
 
-def calc_mean_rmse(x, y):
-    rmses = list(np.sqrt(np.mean(np.square((x - y)), axis=0)))
+def calc_mean_rmse(x, y, samplewise=False):
+    if samplewise:
+        axis = 1
+    else:
+        axis = 0
+    rmses = list(np.sqrt(np.mean(np.square((x - y)), axis=axis)))
     return np.mean(rmses), rmses
 
 
-def calc_mean_rmse_df(df_1, df_2, verbose=2, exclude_cols=None):
+def calc_mean_rmse_df(df_1, df_2, verbose=2, exclude_cols=None, samplewise=False):
     # make sure the columns are the same
     if exclude_cols is not None:
         df_1 = df_1.drop(exclude_cols, axis=1)
         df_2 = df_2.drop(exclude_cols, axis=1)
     df_1 = df_1[df_2.columns]
-    mean_rmse, rmses = calc_mean_rmse(df_1.values, df_2.values)
+    mean_rmse, rmses = calc_mean_rmse(df_1.values, df_2.values, samplewise=samplewise)
     if verbose > 1:
         for k, col in enumerate(df_1.columns):
             print(f"RMSE {col}: {rmses[k]}")
     if verbose > 0:
-            print(f"Mean RMSE: {mean_rmse}")
+        print(f"Mean RMSE: {mean_rmse}")
     df_1 = df_1[df_2.columns]
     return mean_rmse, rmses
+
+
+def calc_metrics_df(df_1, df_2, verbose=1, exclude_cols=None):
+    mean_corr, corrs = calc_mean_corr_df(
+        df_1, df_2, verbose=verbose, exclude_cols=exclude_cols
+    )
+    mean_corr_sample, corrs_sample = calc_mean_corr_df(
+        df_1, df_2, transpose=False, verbose=verbose, exclude_cols=exclude_cols
+    )
+
+    mean_rmse, rmses = calc_mean_rmse_df(
+        df_1, df_2, verbose=verbose, exclude_cols=exclude_cols
+    )
+    mean_rmse_sample, rmses_sample = calc_mean_rmse_df(
+        df_1, df_2, samplewise=True, verbose=verbose, exclude_cols=exclude_cols
+    )
+
+    return (
+        mean_corr,
+        corrs,
+        mean_corr_sample,
+        corrs_sample,
+        mean_rmse,
+        rmses,
+        mean_rmse_sample,
+        rmses_sample,
+    )
