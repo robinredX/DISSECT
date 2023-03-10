b7b2a9ad882ea223f1b73ee3e88a4798967e5db1
dschaub95 <schaub.darius@gmail.com>
2023-03-10 10:03:14 +0000

    add dataset and datamodule for spatial data

---
+++ src/data/datamodules.py
@@ -0,0 +1,115 @@
+from typing import Any, Dict, Optional, Tuple
+
+import torch
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import ConcatDataset, Dataset
+from torch_geometric.loader import DataLoader
+import scanpy as sc
+
+from src.data.datasets import get_spatial_train_and_test_set
+from src.data.utils import load_prepared_data, load_spatial_data
+
+
+class SpatialDataModule(LightningDataModule):
+    def __init__(
+        self,
+        st_path: str,
+        reference_dir: str,
+        radius: float = 0.035,
+        p: float = 0.0,
+        num_samples: int = 32,
+        train_batch_size: int = 1,
+        val_batch_size: int = 1,
+        num_workers: int = 0,
+        pin_memory: bool = True,
+    ) -> None:
+        super().__init__()
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+        
+        self.st_data = load_spatial_data(self.hparams.st_path)
+        self.X_real, self.X_real_train, self.X_sim, self.y_sim = load_prepared_data(
+            self.hparams.reference_dir
+        )
+        # ground truth only available for slideseq data
+        if "slide" in self.hparams.st_path:
+            self.y_real = self.st_data.obs[self.st_data.obs.columns[2::]].to_numpy()
+        else:
+            self.y_real = None
+
+        self.train_data: Optional[Dataset] = None
+        self.val_data: Optional[Dataset] = None
+    
+    @property
+    def num_celltypes(self) -> int:
+        return self.y_sim.shape[1]
+    
+    @property
+    def num_spots(self) -> int:
+        return self.X_real.shape[0]
+
+    def prepare_data(self) -> None:
+        # potentially run data dissect data preparation here
+        # should not be used to set variables like self.train_data
+        pass
+
+    def setup(self, stage: str = None) -> None:
+        """Load data. Set variables: `self.train_data`, `self.val_data`, `self.data_test`.
+
+        This method is called by lightning with both `trainer.fit()` and `trainer.test()`, so be
+        careful not to execute things like random split twice!
+        """
+        # load data only of not done already
+        if not self.train_data and not self.val_data:
+            self.train_data, self.val_data = get_spatial_train_and_test_set(
+                self.st_data,
+                self.X_real,
+                self.X_sim,
+                self.y_sim,
+                self.hparams.radius,
+                p=self.hparams.p,
+                y_real=self.y_real,
+                num_samples=self.hparams.num_samples,
+            )
+            # maybe later also include separate test set
+            # when applying the method to separate test set
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.train_data,
+            batch_size=self.hparams.train_batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+            drop_last=True,
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.val_data,
+            batch_size=self.hparams.val_batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self) -> DataLoader:
+        return self.val_dataloader()
+
+    def predict_dataloader(self) -> DataLoader:
+        return self.val_dataloader()
+    
+    def check_data(self):
+        # initialize internal datasets
+        self.setup()
+        val_loader = self.val_dataloader()
+        train_loader = self.train_dataloader()
+        train_batch = next(iter(train_loader))
+        val_batch = next(iter(val_loader))
+        print(f"Elements in train batch: {len(train_batch)}")
+        print("Train batch element 0:", train_batch[0])
+        print("Train batch element 1:", train_batch[1])
+        print(f"Elements in val batch: {len(val_batch)}")
+        print("Val batch element 0:", val_batch[0])
+        return train_batch, val_batch
---
+++ src/data/datasets.py
@@ -0,0 +1,165 @@
+from torch.utils.data import Dataset
+from torch_geometric.loader import DataLoader
+import torch
+import numpy as np
+import torch_geometric.utils as pyg_utils
+from torch_geometric.data import Data
+
+from src.data.graph_utils import construct_spatial_graph, construct_networkx_graph, normalize_coords
+
+
+def get_spatial_train_and_test_set(
+    st_data, X_real, X_sim, y_sim, radius, p=0.0, y_real=None, num_samples=32
+):
+    train_data = SpatialDISSECTDataset(
+        st_data,
+        X_real,
+        X_sim,
+        y_sim,
+        radius=radius,
+        p=p,
+        num_samples=num_samples,
+    )
+
+    test_data = SpatialDISSECTDataset(
+        st_data,
+        X_real,
+        X_sim,
+        y_sim,
+        num_samples=1,
+        radius=radius,
+        test=True,
+        y_real=y_real,
+    )
+    return train_data, test_data
+
+
+class SpatialDISSECTDataset(Dataset):
+    """
+    Spatial dataset base class.
+    Randomly generates new simulated graphs for each sample.
+    """
+
+    def __init__(
+        self,
+        st_data,
+        X_real,
+        X_sim,
+        y_sim,
+        radius=0.02,
+        p=0.0,
+        test=False,
+        y_real=None,
+        num_samples=32,
+    ):
+        self.num_samples = num_samples
+        self.num_spots = X_real.shape[0]
+        self.num_simulations = X_sim.shape[0]
+        self.radius = radius
+        self.p = p
+        self.test = test
+
+        self.X_sim = torch.Tensor(X_sim)
+        self.num_sims = self.X_sim.shape[0]
+        self.y_sim = torch.Tensor(y_sim)
+
+        self.real_graph = self.construct_real_graph(st_data, X_real, y_real)
+        if not self.test:
+            self.sim_base_graph = self.construct_sim_base_graph()
+
+    def construct_real_graph(self, st_data, X_real, y_real):
+        coords, dist_mat, adj_mat = construct_spatial_graph(
+            st_data.obsm["spatial"].copy(), radius=self.radius
+        )
+        real_graph = construct_networkx_graph(coords, dist_mat, adj_mat, X_real)
+        real_graph = pyg_utils.from_networkx(real_graph)
+        if y_real is not None:
+            real_graph.y = torch.Tensor(y_real)
+        return real_graph
+
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, idx):
+        # get new sim graph
+        if self.test:
+            sim_graph = Data()
+        else:
+            sim_graph = self.construct_new_sim_graph()
+        return [self.real_graph, sim_graph]
+
+    def construct_sim_base_graph(self):
+        # create simulated base graph
+        num_nodes = self.num_spots
+        # create fake coordinates to match dimensionality
+        fake_pos = torch.zeros((num_nodes, 2)) - 1
+
+        # build simulated graph
+        # define custom edge index instead of the spatial one
+        # e.g. fully connected or no connections or random
+        sim_edge_index = pyg_utils.erdos_renyi_graph(num_nodes, self.p, directed=False)
+        # always add self loops
+        loop_index = torch.arange(0, num_nodes, dtype=torch.long)
+        loop_index = loop_index.unsqueeze(0).repeat(2, 1)
+        sim_edge_index = torch.cat([sim_edge_index, loop_index], dim=1)
+        sim_base_graph = Data(x=None, edge_index=sim_edge_index, pos=fake_pos, y=None)
+        return sim_base_graph
+
+    def construct_new_sim_graph(self):
+        # create new simulated graph by updating node features of the base graph
+        sim_slice = np.random.choice(self.num_sims, size=self.num_spots, replace=False)
+        x_sim = self.X_sim[sim_slice, :]
+        y_sim_slice = self.y_sim[sim_slice, :]
+        self.sim_base_graph.x = x_sim
+        self.sim_base_graph.y = y_sim_slice
+        return self.sim_base_graph
+
+
+def prepare_dataset(
+    X_real_train,
+    X_sim,
+    y_sim,
+    y_real=None,
+    st_data=None,
+):
+    size = X_real_train.shape[0]
+    # check whether coordinates available
+    if st_data is not None:
+        coords = normalize_coords(st_data.obsm["spatial"].copy())
+        # repeat along axis 0
+        coords = np.tile(coords, ((size // coords.shape[0]) + 1, 1))[0:size, :]
+        coords = torch.Tensor(coords)
+
+    X_real_train = torch.Tensor(X_real_train)
+    X_sim = torch.Tensor(X_sim)
+    y_sim = torch.Tensor(y_sim)
+    if y_real is not None:
+        y_real = np.tile(y_real, ((size // y_real.shape[0]) + 1, 1))[0:size, :]
+        y_real = torch.Tensor(y_real)
+
+    # convert this into one big datalist
+    data_list = []
+    for i in range(X_real_train.shape[0]):
+        x_real = X_real_train[i, :][None, :]
+        x_sim = X_sim[i, :][None, :]
+        y_sim_sub = y_sim[i, :][None, :]
+
+        # check whether ground truth available for real data
+        if y_real is not None:
+            y_real_sub = y_real[i, :][None, :]
+        else:
+            y_real_sub = None
+
+        # check whether coordinates available
+        if st_data is not None:
+            pos = coords[i, :][None, :]
+            fake_pos = torch.zeros((1, 2)) - 1
+        else:
+            pos = None
+            fake_pos = None
+        g_real = Data(
+            x=x_real, y=y_real_sub, pos=pos, edge_index=None, edge_weight=None
+        )
+        g_sim = Data(x=x_sim, y=y_sim_sub, pos=fake_pos, edge_index=None)
+        data_list.append([g_real, g_sim])
+    return data_list
