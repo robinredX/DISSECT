e3bceccbbc62d24a62e183a3c5c3e2caffcf40ba
dschaub95 <schaub.darius@gmail.com>
2023-03-15 11:05:07 +0100

    refactor multichannel gnn

--- src/models/gnn.py
+++ src/models/gnn.py
@@ -18,36 +18,19 @@ class DissectSpatial(nn.Module):
         self,
         num_celltypes,
         latent_dim,
-        num_encoder_layers=2,
-        num_decoder_layers=1,
         activation="relu",
+        use_pos=True,
         encoder_kwargs={},
         decoder_kwargs={},
-        use_pos=True,
-        fusion="concat",
-        reset_parameters=False,
     ) -> None:
         super().__init__()
         self.encoder = BiChannelGNNEncoder(
-            latent_dim,
-            num_encoder_layers,
-            activation=activation,
-            use_pos=use_pos,
-            fusion=fusion,
-            **encoder_kwargs
+            latent_dim, activation=activation, **encoder_kwargs
         )
         self.decoder = CelltypeDecoder(
-            latent_dim,
-            int(latent_dim / 2),
-            num_celltypes,
-            activation=activation,
-            num_layers=num_decoder_layers,
-            **decoder_kwargs
+            latent_dim, num_celltypes, activation=activation, **decoder_kwargs
         )
         self.use_pos = use_pos
-        # optional custom initialization
-        if reset_parameters:
-            self.reset_parameters()
 
     def forward(
         self, x, edge_index, edge_weight=None, edge_attr=None, pos=None, batch=None
@@ -59,6 +42,7 @@ class DissectSpatial(nn.Module):
         return out
 
     def reset_parameters(self):
+        # requires that all parameters are initialized beforehand
         for w in self.parameters():
             if len(w.shape) == 1:
                 zeros(w)
@@ -70,16 +54,22 @@ class BiChannelGNNEncoder(nn.Module):
     def __init__(
         self,
         latent_dim,
-        num_layers,
-        ff_hidden_dim=256,
-        # batch_size,
-        activation="relu",
+        num_layers=1,
+        activation="relu6",
         dropout=0.0,
-        norm="batch_norm",
+        norm=None,
+        lin_channel=False,
+        spatial_channel=True,
+        num_heads=1,
         spatial_conv_kwargs={},
+        latent_channel=False,
+        knn=6,
+        cosine=False,
         latent_conv_kwargs={},
-        use_pos=True,
-        fusion="concat",
+        fusion=None,
+        use_ffn=False,
+        ff_hidden_dim=256,
+        plain_last=False,
     ) -> None:
         super().__init__()
         self.mlp = MLP(
@@ -93,15 +83,21 @@ class BiChannelGNNEncoder(nn.Module):
             self.layers.append(
                 BiChannelGNNBlock(
                     latent_dim,
-                    ff_hidden_dim,
-                    # batch_size=batch_size,
                     activation=activation,
                     dropout=dropout,
                     norm=norm,
+                    lin_channel=lin_channel,
+                    spatial_channel=spatial_channel,
+                    num_heads=num_heads,
                     spatial_conv_kwargs=spatial_conv_kwargs,
+                    latent_channel=latent_channel,
+                    knn=knn,
+                    cosine=cosine,
                     latent_conv_kwargs=latent_conv_kwargs,
-                    use_pos=use_pos,
                     fusion=fusion,
+                    use_ffn=use_ffn,
+                    ff_hidden_dim=ff_hidden_dim,
+                    plain_last=plain_last,
                 )
             )
 
@@ -119,65 +115,116 @@ class BiChannelGNNBlock(nn.Module):
     def __init__(
         self,
         latent_dim,
-        ff_hidden_dim,
         activation="relu6",
         dropout=0.0,
-        norm="layer",
+        norm=None,
+        lin_channel=False,
+        spatial_channel=True,
         num_heads=1,
         spatial_conv_kwargs={},
-        latent_conv_kwargs={},
+        latent_channel=False,
         knn=6,
         cosine=False,
-        use_pos=True,
-        fusion="concat",
-        sim_layer=False,
+        latent_conv_kwargs={},
+        fusion=None,
+        use_ffn=False,
+        ff_hidden_dim=256,
+        plain_last=False,
     ) -> None:
         super().__init__()
-        self.spatial_conv = GATv2Conv(
-            latent_dim,
-            latent_dim,
-            num_heads,
-            edge_dim=1,
-            add_self_loops=False,
-            **spatial_conv_kwargs
-        )
-        # extra conv with just self loops for simulated data
-        if sim_layer:
-            self.spatial_lin = nn.Linear(latent_dim, latent_dim)
-        self.sim_layer = sim_layer
-        self.activation = activation_resolver(activation)
-        
-        # self.latent_conv = GATv2Conv(
-        #     latent_dim, latent_dim, num_heads, edge_dim=1, **latent_conv_kwargs
-        # )
-        # without latent graph construction
-        # self.latent_conv = MultiheadAttention(
-        #     latent_dim,
-        #     num_heads=num_heads,
-        #     dropout=dropout,
-        #     batch_first=True,
-        #     **latent_conv_kwargs
-        # )
-        
+        self.spatial_channel = spatial_channel
+        if spatial_channel:
+            self.spatial_conv = GATv2Conv(
+                latent_dim,
+                latent_dim,
+                num_heads,
+                edge_dim=1,
+                add_self_loops=False,
+                **spatial_conv_kwargs
+            )
 
+        # extra conv with just self loops for simulated data
+        self.lin_channel = lin_channel
+        if lin_channel:
+            self.lin_layer = nn.Linear(latent_dim, latent_dim)
+
+        self.latent_channel = latent_channel
+        if latent_channel:
+            self.latent_conv = GATv2Conv(
+                latent_dim, latent_dim, num_heads, edge_dim=1, **latent_conv_kwargs
+            )
+        # make sure some channel is selected
+        num_channels = sum([spatial_channel, lin_channel, latent_channel])
+        assert num_channels > 0, "No channels selected"
+
+        self.fusion = fusion
         if fusion == "concat":
             self.concat_linear = nn.LazyLinear(latent_dim)
         elif fusion == "gating":
-            self.gate = GatingUnit(num_inputs=2)
-        self.ffn = FeedForwardBlock(
-            latent_dim, ff_hidden_dim, activation=activation, dropout=dropout
-        )
+            self.gating_unit = GatingUnit(num_inputs=num_channels)
 
+        if use_ffn:
+            self.ffn = FeedForwardBlock(
+                latent_dim, ff_hidden_dim, activation=activation, dropout=dropout
+            )
+        self.use_ffn = use_ffn
+
+        self.activation = activation_resolver(activation)
         if norm is None:
             self.norm1 = Identity()
             self.norm2 = Identity()
         else:
             self.norm1 = normalization_resolver(norm, in_channels=latent_dim)
             self.norm2 = normalization_resolver(norm, in_channels=latent_dim)
+
         self.knn = knn
         self.cosine = cosine
-        self.use_pos = use_pos
-        self.fusion = fusion
+        self.plain_last = plain_last
+
+    def forward(
+        self, x, edge_index, edge_weight=None, edge_attr=None, pos=None, batch=None
+    ):
+        # x.shape = (batch*num_nodes, latent_dim)
+
+        # TODO: check if conv takes edge weights or edge attributes
+        channels = []
+        if self.spatial_channel:
+            channels.append(self.spatial_conv(x, edge_index, edge_attr=edge_attr))
+            # might need projection back to latent dim
+        if self.lin_channel:
+            channels.append(self.lin_layer(x))
+        if self.latent_channel:
+            latent_edge_index = self.construct_latent_graph(x, batch)
+            out_latent = self.latent_conv(x, latent_edge_index)
+            # might need projection back to latent dim
+            channels.append(out_latent)
+
+        # fusion should be dependent on the type of the input: real, sim, or mix
+        # maybe input extra onehot encoding of type of input
+
+        if self.fusion == "gating":
+            out_fusion = self.gating_unit(x, *channels)
+        elif self.fusion == "concat":
+            # concat fusion
+            # concat gnn channels, perform linear transformation, and add initial input
+            out_concat = torch.cat(channels, dim=-1)
+            out_fusion = self.concat_linear(out_concat)
+            out_fusion = out_fusion + x
+        else:
+            out_fusion = channels[0]
+
+        # perform normalization and feed forward
+        if self.use_ffn:
+            out = self.norm1(out_fusion)
+            out_ffn = self.ffn(out)
+            out = self.norm2(out + out_ffn)
+        else:
+            out = out_fusion
+
+        if not self.plain_last:
+            out = self.activation(out)
+
+        return out
 
     def construct_latent_graph(self, x, batch):
         # compute pairwise distances between all embeddings
@@ -192,54 +239,22 @@ class BiChannelGNNBlock(nn.Module):
         )
         return edge_index
 
-    def forward(
-        self, x, edge_index, edge_weight=None, edge_attr=None, pos=None, batch=None
-    ):
-        # x.shape = (batch*num_nodes, latent_dim)
-
-        # perform spatial and latent GNN computation
-        # latent_edge_index = self.construct_latent_graph(x, batch)
-        # out_latent = self.latent_conv(x, latent_edge_index)
-
-        # TODO: check if conv takes edge weights or edge attributes
-        if self.sim_layer and edge_attr is None:
-            out_spatial = self.spatial_lin(x)
-        else:
-            out_spatial = self.spatial_conv(x, edge_index, edge_attr=edge_attr)
-
-        if self.fusion == "gating":
-            out = self.gate(x, out_spatial)
-        elif self.fusion == "concat":
-            # concat fusion
-            # concat gnn outputs, perform linear transformation, and add initial input
-            out_concat = torch.cat([out_spatial], dim=-1)
-            out_multi = self.concat_linear(out_concat)
-            out = x + out_multi
-        else:
-            out = out_spatial
-            out = self.activation(out)
-
-        # perform normalization and feed forward
-        out = self.norm1(out)
-        out_ffn = self.ffn(out)
-        out = self.norm2(out + out_ffn)
-
-        return out
-
 
 class GatingUnit(nn.Module):
-    def __init__(self, num_inputs=2) -> None:
+    def __init__(self, num_channels) -> None:
         super().__init__()
-        self.linear = nn.LazyLinear(num_inputs)
+        self.num_channels = num_channels
+        self.linear = nn.LazyLinear(num_channels + 1)
         self.softmax = nn.Softmax(dim=-1)
 
-    def forward(self, x, *args):
-        gate = self.linear(x)
-        gate = self.softmax(gate)
+    def forward(self, x, *channels):
+        # compute weights based on previouse latent representation
+        logits = self.linear(x)
+        weights = self.softmax(logits)
         # compute weighted sum
-        out = gate[:, [0]] * x
-        for i in range(1, len(args)):
-            out += gate[:, [i]] * args[i]
+        out = weights[:, [0]] * x
+        for i in range(1, self.num_channels + 1):
+            out += weights[:, [i]] * channels[i]
         return out
 
 
@@ -264,9 +279,9 @@ class CelltypeDecoder(nn.Module):
     def __init__(
         self,
         in_channels,
-        hidden_channels,
         num_celltypes,
-        num_layers=1,
+        num_layers=2,
+        hidden_channels=64,
         activation="relu",
         norm=None,
         **kwargs
